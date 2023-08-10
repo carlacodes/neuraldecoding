8bdc0c03b787e4c614570b81b3dd1a8af7f2c054
carlacodes <carla.griffiths@gmail.com>
2023-08-10 16:01:44 +0100

    making multipanel and running loop

--- visualisation/plotdecodingscoresovetime.py
+++ visualisation/plotdecodingscoresovetime.py
@@ -10,24 +10,32 @@ def run_scores_and_plot(file_path, pitchshift, output_folder, ferretname,  strin
         file_path + '/' + r'scores_' + ferretname + '_2022_' + stringprobewordindex + '_' + ferretname + '_probe_'+pitchshift +'_'+talker_string+'_bs.npy',
         allow_pickle=True)[()]
 
+    if stringprobewordindex == '2':
+        word_equivalent = 'when a'
+
     #for each cluster plot their scores over time
     index = -1
+    #make a subplot grid
+
+    fig, axs = plt.subplots(2, int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2), figsize=(50,15))
     for cluster in scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id']:
         #get the scores
         index = index + 1
         cluster_scores =scores['talker1']['target_vs_probe']['nopitchshift']['lstm_balancedaccuracylist'][index]
         #get the timepoints
         timepoints = np.arange(0, len(cluster_scores)/100, 0.01)
+        if index < int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2):
+            ax = axs[1,index]
+        else:
+            ax = axs[0,index-int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2)]
 
-        fig, ax = plt.subplots()
         ax.plot(timepoints, cluster_scores)
         ax.set(xlabel='time since target word (s)', ylabel='balanced accuracy',
-                title='balanced accuracy over time for cluster ' + str(cluster) + ' \n for ' + ferretname + ' ' + pitchshift + ' ' + talker)
-        ax.grid()
+            title='cluster ' + str(cluster))
         ax.set_ylim([0, 1])
-
-        fig.savefig(output_folder + '/' + 'cluster' + str(cluster) + '.png')
-        plt.show()
+        ax.grid()
+    plt.suptitle('LSTM balanced accuracy for ' + ferretname + ' ' + pitchshift + ' ' + talker+ ' target vs. ' + word_equivalent, fontsize = 30)
+    fig.savefig(output_folder + '/' + 'multipanel' +stringprobewordindex + '.png', bbox_inches='tight')
 
 
     return scores
@@ -45,5 +53,8 @@ if __name__ == '__main__':
     output_folder = 'E:\\lstm_kfold_01082023_oreovertimes2'
     ferretname = 'orecchiette'
     pitchshift = 'nopitchshift'
-    run_scores_and_plot(file_path, pitchshift, output_folder, ferretname, stringprobewordindex=str(2), talker = 'female' )
+    stringprobewordlist = [5,2,42,32,20]
+    # probewordlist = [ (5, 6),(2, 2), (42, 49), (32, 38), (20, 22)]
+    for stringprobewordindex in stringprobewordlist:
+        run_scores_and_plot(file_path, pitchshift, output_folder, ferretname, stringprobewordindex=str(stringprobewordindex), talker = 'female' )
 
