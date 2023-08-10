4858181c0b4da475b727f1636a0110e179d6059e
carlacodes <carla.griffiths@gmail.com>
2023-08-10 17:23:57 +0100

    looping across talker

--- visualisation/plotdecodingscoresovetime.py
+++ visualisation/plotdecodingscoresovetime.py
@@ -3,25 +3,42 @@ import matplotlib.pyplot as plt
 
 
 
-def run_scores_and_plot(file_path, pitchshift, output_folder, ferretname,  stringprobewordindex=str(2), talker='female'):
+def run_scores_and_plot(file_path, pitchshift, output_folder, ferretname,  stringprobewordindex=str(2), talker='female', totalcount = 0):
     if talker == 'female':
         talker_string = 'onlyfemaletalker'
+        talkerinput = 'talker1'
+    else:
+        talker_string = 'onlymaletalker'
+        talkerinput = 'talker2'
     scores = np.load(
         file_path + '/' + r'scores_' + ferretname + '_2022_' + stringprobewordindex + '_' + ferretname + '_probe_'+pitchshift +'_'+talker_string+'_bs.npy',
         allow_pickle=True)[()]
+    if pitchshift == 'pitchshift':
+        pitchshifttext = 'intra-roved F0'
+    elif pitchshift == 'nopitchshift':
+        pitchshifttext = 'control F0'
 
     if stringprobewordindex == '2':
         word_equivalent = 'when a'
+    elif stringprobewordindex == '5':
+        word_equivalent = 'craft'
+    elif stringprobewordindex == '42':
+        word_equivalent = 'accurate'
+    elif stringprobewordindex == '32':
+        word_equivalent = 'of science'
+    elif stringprobewordindex == '22':
+        word_equivalent = 'in contrast'
+
 
     #for each cluster plot their scores over time
     index = -1
     #make a subplot grid
-
-    fig, axs = plt.subplots(2, int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2), figsize=(50,15))
-    for cluster in scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id']:
+    color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    fig, axs = plt.subplots(2, int(len(scores[talkerinput]['target_vs_probe'][pitchshift]['cluster_id'])/2), figsize=(50,15))
+    for cluster in scores[talkerinput]['target_vs_probe'][pitchshift]['cluster_id']:
         #get the scores
         index = index + 1
-        cluster_scores =scores['talker1']['target_vs_probe']['nopitchshift']['lstm_balancedaccuracylist'][index]
+        cluster_scores =scores[talkerinput]['target_vs_probe']['nopitchshift']['lstm_balancedaccuracylist'][index]
         #get the timepoints
         timepoints = np.arange(0, len(cluster_scores)/100, 0.01)
         if index < int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2):
@@ -29,13 +46,13 @@ def run_scores_and_plot(file_path, pitchshift, output_folder, ferretname,  strin
         else:
             ax = axs[0,index-int(len(scores['talker1']['target_vs_probe']['nopitchshift']['cluster_id'])/2)]
 
-        ax.plot(timepoints, cluster_scores)
+        ax.plot(timepoints, cluster_scores, c = color_list[totalcount])
         ax.set(xlabel='time since target word (s)', ylabel='balanced accuracy',
-            title='cluster ' + str(cluster))
+            title='unit ' + str(cluster))
         ax.set_ylim([0, 1])
         ax.grid()
-    plt.suptitle('LSTM balanced accuracy for ' + ferretname + ' ' + pitchshift + ' ' + talker+ ' target vs. ' + word_equivalent, fontsize = 30)
-    fig.savefig(output_folder + '/' + 'multipanel' +stringprobewordindex + '.png', bbox_inches='tight')
+    plt.suptitle('LSTM balanced accuracy for ' + ferretname + ' ' + pitchshifttext + ' ' + talker+ ' target vs. ' + word_equivalent, fontsize = 30)
+    fig.savefig(output_folder + '/' + 'multipanel' + stringprobewordindex +'talker_'+talker+ '.png', bbox_inches='tight')
 
 
     return scores
@@ -53,8 +70,14 @@ if __name__ == '__main__':
     output_folder = 'E:\\lstm_kfold_01082023_oreovertimes2'
     ferretname = 'orecchiette'
     pitchshift = 'nopitchshift'
-    stringprobewordlist = [5,2,42,32,20]
+    stringprobewordlist = [5,2]
     # probewordlist = [ (5, 6),(2, 2), (42, 49), (32, 38), (20, 22)]
-    for stringprobewordindex in stringprobewordlist:
-        run_scores_and_plot(file_path, pitchshift, output_folder, ferretname, stringprobewordindex=str(stringprobewordindex), talker = 'female' )
+    totalcount = 0
+    talkerlist = ['female', 'male']
+    for talker in talkerlist:
+        for probeword in stringprobewordlist:
+
+            print(probeword)
+            run_scores_and_plot(file_path, pitchshift, output_folder, ferretname, stringprobewordindex=str(probeword), talker = talker, totalcount = totalcount )
+            totalcount = totalcount + 1
 
