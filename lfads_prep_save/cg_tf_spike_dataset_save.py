d265ebefe43b90f00350308897b5e2c01d854811
carlacodes <carla.griffiths@gmail.com>
2023-08-30 16:27:39 +0100

    cleaning

--- lfads_prep_save/cg_tf_spike_dataset_save.py
+++ lfads_prep_save/cg_tf_spike_dataset_save.py
@@ -9,18 +9,14 @@ import pickle
 
 
 
-def target_vs_probe(blocks, talker=1, probewords=[20, 22], pitchshift=True):
-    # datapath = Path('/Users/juleslebert/home/phd/fens_data/warp_data/Trifle_June_2022/Trifle_week_16_05_22
-    # /mountainsort4/phy') fname = 'blocks.pkl' with open(datapath / 'blocks.pkl', 'rb') as f: blocks = pickle.load(f)
+def generatewordspiketrains(blocks, talker=1, probewords=[20, 22], pitchshift=True, window = [0, 0.6]):
+
     if talker == 1:
         probeword = probewords[0]
     else:
         probeword = probewords[1]
     binsize = 0.01
-    window = [0, 0.6]
 
-    epochs = ['Early', 'Late']
-    epoch_threshold = 1.5
     clust_ids = [st.annotations['cluster_id'] for st in blocks[0].segments[0].spiketrains if
                  st.annotations['group'] != 'noise']
 
@@ -35,6 +31,7 @@ def target_vs_probe(blocks, talker=1, probewords=[20, 22], pitchshift=True):
               'lstm_score_list': [],
               'history': [],}
     cluster_id_droplist = np.empty([])
+    raster_reshaped_array = []
     for cluster_id in tqdm(clust_ids):
 
 
@@ -64,24 +61,24 @@ def target_vs_probe(blocks, talker=1, probewords=[20, 22], pitchshift=True):
             count+=1
 
 
-        stim1 = np.full(len(raster_probe_reshaped), 1)  # 1 = probe word
-        # if len(stim0)+len(stim1)<7:
-        #     print('less than 10 trials')
-        #     continue
 
         raster_lstm = raster_probe_reshaped
 
         raster_reshaped = np.reshape(raster_lstm, (np.size(raster_lstm, 0), np.size(raster_lstm, 1), 1)).astype(
             'float32')
+        raster_reshaped_array.append(raster_reshaped)
 
         #save as h5 files
-        print('saving h5 files')
-        with h5py.File(f'D:/tf_h5files/F1702_Zola/raster_reshaped_{str(talker)}_{str(probeword)}_pitchshift_{pitchshift}.h5', 'w') as hf:
-            hf.create_dataset("spike_data", data=raster_reshaped)
+    print('saving h5 files')
+
+
+    raster_reshaped_array_final= np.stack(raster_reshaped_array)
+    with h5py.File(f'D:/tf_h5files/F1702_Zola/raster_reshaped_{str(talker)}_{str(probeword)}_pitchshift_{pitchshift}.h5', 'w') as hf:
+        hf.create_dataset("spike_data", data=raster_reshaped_array_final)
             # hf.create_dataset("stim_data", data=stim_reshaped)
 
 
-    return scores
+    return
 
 
 
@@ -112,56 +109,10 @@ def run_export(dir):
                 window = [0, 0.6]
             else:
                 window = [0, 0.5]
-            # window=[0,0.87]
-            print(f'talker {talker}')
-
-            scores[f'talker{talker}'] = {}
-
-            scores[f'talker{talker}']['target_vs_probe'] = {}
-
-            scores[f'talker{talker}']['target_vs_probe']['nopitchshift'] = target_vs_probe(blocks, talker=talker,
-                                                                                           probewords=probeword,
-                                                                                           pitchshift=False)
-            scores[f'talker{talker}']['target_vs_probe']['pitchshift'] = target_vs_probe(blocks, talker=talker,
-                                                                                         probewords=probeword,
-                                                                                         pitchshift=True)
 
 
-        #     np.save(saveDir / f'scores_{dir}_{probeword[0]}_zola_probe_pitchshift_vs_not_by_talker_bs.npy', scores)
-        #
-        # fname = 'scores_' + dir + f'_probe_earlylate_left_right_win_bs_{binsize}'
-        # save_pdf_classification_lstm(scores, saveDir, fname, probeword)
-
-    # title = f'eucl_classification_{dir}_26082022'
-    # with PdfPages(saveDir / f'{title}.pdf') as pdf:
-    #     for i, clus in enumerate(tqdm(scores['probe_early_vs_late']['cluster_id'])):
-    #         fig, axes = plt.subplots(3, 1, figsize=(4,10), dpi=300)
-    #         for j, comp in enumerate(scores):
-    #             ax = axes[j]
-    #             sns.heatmap(scores[comp]['cm'][i], annot=True, fmt="d", ax=ax, cmap='Blues')
-    #             ax.set_xlabel('\nPredicted Values')
-    #             ax.set_ylabel('Actual Values ');
-    #             ax.set_title(comp)
-
-    #         y = [scores[comp]['score'][i][0] for comp in scores]
-    #         yerrmax = [scores[comp]['score'][i][1] for comp in scores]
-    #         yerrmin = [scores[comp]['score'][i][2] for comp in scores]
-    #         axes[-1].bar(range(len(scores)), y, align='center', color='cornflowerblue')
-    #         axes[-1].set_xticks(range(len(scores)))
-    #         axes[-1].set_xticklabels(list(scores.keys()))
-
-    #         axes[-1].scatter(range(len(scores)), yerrmax, c='black', marker='_', s=10)
-    #         axes[-1].scatter(range(len(scores)), yerrmin, c='black', marker='_', s=10)
-
-    #         axes[-1].set_ylim([0,1])
-
-    #         # plt.figure()
-    #         # plt.imshow(scores[clus]['cm'], interpolation='nearest', cmap=plt.cm.Blues)
-    #         fig.suptitle(f'cluster {clus}')
-    #         fig.tight_layout()
-    #         # plt.colorbar()
-    #         pdf.savefig(fig)
-    #         plt.close(fig)
+            generatewordspiketrains(blocks, talker=talker, probewords=probeword,pitchshift=False, window = window )
+            generatewordspiketrains(blocks, talker=talker, probewords=probeword, pitchshift=True, window = window)
 
 
 def main():
