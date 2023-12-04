6298b99024111b4f3fb236e6fc445da58509fd31
carlacodes <carla.griffiths@gmail.com>
2023-12-04 11:36:43 +0000

    making sure only high performing units are shown

--- visualisation/helpers/vis_stats_helpers.py
+++ visualisation/helpers/vis_stats_helpers.py
@@ -103,10 +103,11 @@ def run_mixed_effects_on_dataframe(combined_df):
         combined_df.loc[combined_df['ID'] == unit_id, 'FerretID'] = ferret_id
 
 
-    # model_formula = "Score ~ Naive +  PitchShift + (Naive * PitchShift )+ ProbeWord"
+    model_formula = "Score ~ Naive +  PitchShift + (Naive * PitchShift )+ ProbeWord"
     model_formula2 = "Score ~ Naive +  PitchShift + (Naive * PitchShift)"
+    model_formula3 = "Score ~ Naive +  PitchShift + (Naive * PitchShift) +BrainArea + ProbeWord"
 
-    mixed_model = smf.mixedlm(model_formula2, data=combined_df, groups=combined_df['FerretID'])
+    mixed_model = smf.mixedlm(model_formula, data=combined_df, groups=combined_df['FerretID'])
 
     # Fit the model
     result = mixed_model.fit()
