9cb4522687972cb219d082b3390f122549968657
carlacodes <carla.griffiths@gmail.com>
2024-03-12 11:14:58 +0000

    typo in the csv file that is carried over in my code

--- visualisation/loadscoresandvisualise_trainedvsnaive_testonrove_inter_onlyhighgenunits_02112023.py
+++ visualisation/loadscoresandvisualise_trainedvsnaive_testonrove_inter_onlyhighgenunits_02112023.py
@@ -172,9 +172,9 @@ def scatterplot_and_visualise(probewordlist,
         singleunitlist_copy = [x for x in singleunitlist_copy if x not in original_to_split_cluster_ids]
         multiunitlist_copy = [x for x in multiunitlist_copy if x not in original_to_split_cluster_ids]
         if fullid == 'F2003_Orecchiette' or fullid == 'F1901_Crumble' or fullid == 'F1812_Nala' or fullid == 'F1902_Eclair':
-            high_units = pd.read_csv(f'G:/neural_chapter/figures/unit_ids_naive_topgenindex_{fullid}.csv')
+            high_units = pd.read_csv(f'G:/neural_chapter/figures/genfracunitidcsvs_new/unit_ids_trained_topgenindex_{fullid}.csv')
         else:
-            high_units = pd.read_csv(f'G:/neural_chapter/figures/unit_ids_trained_topgenindex_{fullid}.csv')
+            high_units = pd.read_csv(f'G:/neural_chapter/figures/genfracunitidcsvs_new/unit_ids_trained_topgenindex_{fullid}.csv')
 
 
         high_units = high_units[(high_units['rec_name'] == recname) & (high_units['stream'] == stream_id)]
@@ -809,19 +809,6 @@ def generate_plots(dictlist, dictlist_trained, dictlist_naive, labels, colors):
     ax.legend()
     plt.savefig('G:/testonroveresults/results_testonrove_inter_28102023/scatter_lstm_decoding_score_for_trained_and_naive_animals_251versus_control.png', dpi = 300, bbox_inches = 'tight')
     plt.show()
-    #now only repeat with the high performing units
-    for ferret_id_fancy in ['F1702_Zola', 'F1815_Cruella', 'F1604_Squinty', 'F1606_Windolene']:
-
-        high_units = pd.read_csv(f'G:/neural_chapter/figures/unit_ids_trained_topgenindex_{ferret_id_fancy}.csv')
-        # remove trailing steam
-
-
-
-        high_units = high_units[(high_units['rec_name'] == rec_name) & (high_units['stream'] == stream)]
-        clust_ids = high_units['ID'].to_list()
-        brain_area = high_units['BrainArea'].to_list()
-
-    #plot with error b
 
 
     return
