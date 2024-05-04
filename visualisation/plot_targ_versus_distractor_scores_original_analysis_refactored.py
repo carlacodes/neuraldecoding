48d2ebc7decebc0afe1332ea105e47e64c07c6dc
carlacodes <carla.griffiths@gmail.com>
2024-05-04 15:58:07 +0100

    changing dates on pngs

--- visualisation/plot_targ_versus_distractor_scores_original_analysis_refactored.py
+++ visualisation/plot_targ_versus_distractor_scores_original_analysis_refactored.py
@@ -701,6 +701,8 @@ def main():
         df_merged['Below-chance'] = df_merged['score'] < df_merged['score_permutation']
         #rename brain area to BrainArea
         df_merged = df_merged.rename(columns={'brain_area': 'BrainArea'})
+        df_merged = df_merged.rename(columns={'score': 'Score'})
+        df_merged = df_merged.rename(columns={'probeword1': 'ProbeWord'})
     plot_major_analysis(df_merged)
     return df_merged
 def color_by_probeword(probeword):
