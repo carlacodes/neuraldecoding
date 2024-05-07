3bf3dff6a6787572566195ef097694b671aff201
carlacodes <carla.griffiths@gmail.com>
2024-05-07 10:30:51 +0100

    rounding for both sub plot title and big figure title

--- cv_loocv_lstmdecoder/cruella_example_rasters_andpsth_grid_readfromcsv.py
+++ cv_loocv_lstmdecoder/cruella_example_rasters_andpsth_grid_readfromcsv.py
@@ -291,6 +291,8 @@ def target_vs_probe_with_raster(blocks, talker=1,  clust_ids = [], stream = 'BB_
                             ax[idx, 0].set_ylabel('trial number')
 
                         ax[idx, 0].set_xlim(custom_xlim)
+                        individual_score = round(individual_score, 2)
+                        individual_perm_score = round(individual_perm_score, 2)
                         ax[idx, 0].set_title(f'Unit: {cluster_id}_{phydir}, \n {animal_id_num}, score: {individual_score}, perm score: {individual_perm_score}')
 
                         ax[idx, 0].text(-0.2, 0.5, probeword_text, horizontalalignment='center',
