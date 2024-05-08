d6b49b6795fd04c2889a6899419e13d19c3e5555
carlacodes <carla.griffiths@gmail.com>
2024-05-08 10:17:22 +0100

    removing unused functions

--- visualisation/plot_targ_versus_distractor_scores_original_analysis_refactored.py
+++ visualisation/plot_targ_versus_distractor_scores_original_analysis_refactored.py
@@ -328,36 +328,6 @@ def load_scores_and_filter(probewordlist,
 
 
 
-def data_merge(a, b):
-    """merges b into a and return merged result
-
-    NOTE: tuples and arbitrary objects are not handled as it is totally ambiguous what should happen"""
-    key = None
-    # ## debug output
-    # sys.stderr.write("DEBUG: %s to %s\n" %(b,a))
-    if a is None or isinstance(a, str) or isinstance(a, int) or isinstance(a, float):
-        # border case for first run or if a is a primitive
-        a = b
-    elif isinstance(a, list):
-        # lists can be only appended
-        if isinstance(b, list):
-            # merge lists
-            a.extend(b)
-        else:
-            # append to list
-            a.append(b)
-    elif isinstance(a, dict):
-        # dicts must be merged
-        if isinstance(b, dict):
-            for key in b:
-                if key in a:
-                    a[key] = data_merge(a[key], b[key])
-                else:
-                    a[key] = b[key]
-
-    return a
-
-
 def runboostedregressiontreeforlstmscore(df_use):
     col = 'score'
     dfx = df_use.loc[:, df_use.columns != col]
